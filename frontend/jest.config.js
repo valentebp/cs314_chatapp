19c0c49d8dc4ff54a39634601aa00fe120bfef41
Valente <102696008+valentebp@users.noreply.github.com>
2026-03-10 23:46:47 -0700

    init frontend structure

---
+++ frontend/jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterFramework: ['<rootDir>/jest.setup.js'],
+  testMatch: ['<rootDir>/tst/**/*.test.{ts,tsx}'],
+  moduleNameMapper: {
+    '\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\.(png|jpg|jpeg|gif|svg|ico)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  transform: {
+    '^.+\.[jt]sx?$': 'babel-jest',
+  },
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/main.tsx',
+    '!tst/**',
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 60,
+      branches: 50,
+      functions: 60,
+      lines: 60,
+    },
+  },
+};
